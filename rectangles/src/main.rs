4348b41b8c48a5c72314acde755008a01b9a1fd4
kota <85015574+tiger-sea@users.noreply.github.com>
2022-08-29 12:57:33 +0900

    fix: use the impl

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,17 +1,34 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32
 }
 
-fn main() {
-    let rec = Rectangle {
-        width: 10,
-        height: 5
-    };
-    // 引数は参照で渡す,所有権を渡したくないから.
-    println!("The area of the rectangle is {}", area(&rec));
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+    
+    fn can_hold(&self, rec: &Rectangle) -> bool {
+        self.width > rec.width && self.height > rec.height
+    }
 }
 
-fn area(dementions: &Rectangle) -> u32 {
-    dementions.height * dementions.width
+// これは関連関数(引数に&selfを取らない関数)でメソッドではない
+#[allow(unused)]
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { height: size, width: size }
+    }
+}
+
+fn main() {
+    let rec1 = Rectangle { width: 10, height: 5 };
+    let rec2 = Rectangle { width: 2, height: 2 };
+    let rec3 = Rectangle { width: 100, height: 100 };
+    // 引数は参照で渡す,所有権を渡したくないから.
+    println!("The area of the rectangle is {}", rec1.area());
+    // println!("Rec1 is {:#?}", rec1); // for debug
+    println!("Can rec1 hold rec2? {}", rec1.can_hold(&rec2));
+    println!("Can rec1 hold rec3? {}", rec1.can_hold(&rec3));
 }
